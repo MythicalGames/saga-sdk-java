6ea14cde42b4e1ad9df3fa4de76fd922d84be08d
Erik Larson <erik@mytical.games>
2022-03-21 09:51:18 -0500

    Add getItemsForPlayer

--- saga-sdk-java/src/main/java/games/mythical/saga/sdk/client/SagaItemClient.java
+++ saga-sdk-java/src/main/java/games/mythical/saga/sdk/client/SagaItemClient.java
@@ -13,6 +13,7 @@ import games.mythical.saga.sdk.proto.common.FilterConditional;
 import games.mythical.saga.sdk.proto.common.Finalized;
 import games.mythical.saga.sdk.proto.streams.StatusStreamGrpc;
 import games.mythical.saga.sdk.proto.streams.Subscribe;
+import games.mythical.saga.sdk.util.Validate;
 import io.grpc.Status;
 import io.grpc.StatusRuntimeException;
 import lombok.extern.slf4j.Slf4j;
@@ -100,8 +101,23 @@ public class SagaItemClient extends AbstractSagaClient {
 
             throw SagaException.fromGrpcException(e);
         }
+    }
 
-
+    public List<SagaItem> getItemsForPlayer(String oauthId) throws SagaException {
+        Validate.notBlank(oauthId, "oauthId is a required value");
+        var request = GetItemsForPlayerRequest.newBuilder()
+            .setTitleId(config.getTitleId())
+            .setOauthId(oauthId)
+            .build();
+        try {
+            var items = serviceBlockingStub.getItemsForPlayer(request);
+            return items.getItemsList().stream().map(SagaItem::fromProto).collect(Collectors.toList());
+        } catch (StatusRuntimeException e) {
+            if (e.getStatus() == Status.NOT_FOUND) {
+                return List.of();
+            }
+            throw SagaException.fromGrpcException(e);
+        }
     }
 
     public void issueItem(String gameInventoryId,
