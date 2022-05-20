ec4dfc56cdcc998cdc267a6daec3550714bf731c
rudy-mythical <75645328+rudy-mythical@users.noreply.github.com>
2022-05-20 14:27:09 -0700

    Fixed unit test

--- saga-sdk-java/src/test/java/games/mythical/saga/sdk/client/SagaItemClientTest.java
+++ saga-sdk-java/src/test/java/games/mythical/saga/sdk/client/SagaItemClientTest.java
@@ -73,8 +73,8 @@ class SagaItemClientTest extends AbstractClientTest {
                 .setTraceId(RandomStringUtils.randomAlphanumeric(30))
                 .setMetadata(SagaMetadata.toProto(generateItemMetadata()))
                 .setItemState(ItemState.forNumber(RandomUtils.nextInt(0, ItemState.values().length - 1)))
-                .setCreatedTimestamp(Instant.now().toEpochMilli() - 86400)
-                .setUpdatedTimestamp(Instant.now().toEpochMilli())
+                .setCreatedAt(Instant.now().toEpochMilli() - 86400)
+                .setUpdatedAt(Instant.now().toEpochMilli())
                 .build();
         when(mockServiceBlockingStub.getItem(any())).thenReturn(expectedResponse);
         var itemResponse = itemClient.getItem(GAME_INVENTORY_ID, false);
