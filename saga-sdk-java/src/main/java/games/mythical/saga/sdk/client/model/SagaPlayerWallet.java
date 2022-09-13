8a8ba5393266988aa04b15514c4b6c51a809b570
Ian Dorion <ian.dorion@mythical.games>
2022-09-13 15:58:09 -0700

    MYTHOS-900 added created_at and updated_at in player wallet response

--- saga-sdk-java/src/main/java/games/mythical/saga/sdk/client/model/SagaPlayerWallet.java
+++ saga-sdk-java/src/main/java/games/mythical/saga/sdk/client/model/SagaPlayerWallet.java
@@ -2,11 +2,13 @@ package games.mythical.saga.sdk.client.model;
 
 import games.mythical.proto_util.ProtoUtil;
 import games.mythical.saga.sdk.proto.api.playerwallet.PlayerWalletProto;
+import games.mythical.saga.sdk.util.ConversionUtils;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
+import games.mythical.proto_util.dto.DtoExclude;
+import java.time.Instant;
 @Data
 @Builder
 @AllArgsConstructor
@@ -16,8 +18,20 @@ public class SagaPlayerWallet {
     private String oauthId;
     private String address;
     private String balanceInWei;
+    @DtoExclude
+    private Instant createdAt;
+    @DtoExclude
+    private Instant updatedAt;
 
     public static SagaPlayerWallet fromProto(PlayerWalletProto proto) {
-        return ProtoUtil.toDto(proto, SagaPlayerWallet.class);
+        var playerwallet = ProtoUtil.toDto(proto, SagaPlayerWallet.class);
+
+        var createdAt = ConversionUtils.protoTimestampToInstant(proto.getCreatedAt());
+        playerwallet.setCreatedAt(createdAt);
+
+        var updatedAt = ConversionUtils.protoTimestampToInstant(proto.getUpdatedAt());
+        playerwallet.setUpdatedAt(updatedAt);
+
+        return playerwallet;
     }
 }
