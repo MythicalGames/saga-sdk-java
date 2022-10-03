70b9ed040fddefffacff9786dfa538e24453b99f
Cristian Traistaru <cristian@mythical.games>
2022-10-03 11:22:33 -0500

    Add stream ID and replay since (#120)

--- saga-sdk-java/src/main/java/games/mythical/saga/sdk/config/SagaSdkConfig.java
+++ saga-sdk-java/src/main/java/games/mythical/saga/sdk/config/SagaSdkConfig.java
@@ -3,26 +3,28 @@ package games.mythical.saga.sdk.config;
 import lombok.Builder;
 import lombok.Getter;
 
+import java.time.Instant;
+
 @Getter
-@Builder(builderClassName = "SagaSdkConfigBuilder")
+@Builder
 public class SagaSdkConfig {
-    private final String authUrl;
+    @Builder.Default
+    private final String authUrl = Constants.DEFAULT_AUTH_URL;
     private final String titleId;
     private final String titleSecret;
-    private final String host;
-    private final int port;
-    private final int keepAlive;
-    private final int tokenRefresh;
-    private final boolean plainText;
-    private final boolean authenticated;
-
-    public static class SagaSdkConfigBuilder {
-        private String authUrl = Constants.DEFAULT_AUTH_URL;
-        private String host = Constants.DEFAULT_GATEWAY_HOST;
-        private int port = Constants.DEFAULT_GATEWAY_PORT;
-        private int keepAlive = Constants.DEFAULT_KEEPALIVE_IN_SECONDS;
-        private int tokenRefresh = Constants.DEFAULT_TOKEN_REFRESH_INTERVAL_IN_SECONDS;
-        private boolean plainText = false;
-        private boolean authenticated = true;
-    }
+    @Builder.Default
+    private final String host = Constants.DEFAULT_GATEWAY_HOST;
+    @Builder.Default
+    private final int port = Constants.DEFAULT_GATEWAY_PORT;
+    @Builder.Default
+    private final int keepAlive = Constants.DEFAULT_KEEPALIVE_IN_SECONDS;
+    @Builder.Default
+    private final int tokenRefresh = Constants.DEFAULT_TOKEN_REFRESH_INTERVAL_IN_SECONDS;
+    @Builder.Default
+    private final boolean plainText = false;
+    @Builder.Default
+    private final boolean authenticated = true;
+    private final String streamId;
+    @Builder.Default
+    private final Instant streamReplaySince = null;
 }
