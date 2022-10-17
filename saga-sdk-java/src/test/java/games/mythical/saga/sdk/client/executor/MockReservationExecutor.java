f4ed0e31197bbb9418c9902859f5d1eae0ef69c5
Cristian Traistaru <cristian@mythical.games>
2022-10-17 16:05:33 -0500

    Add items to reservation redeemed event

--- saga-sdk-java/src/test/java/games/mythical/saga/sdk/client/executor/MockReservationExecutor.java
+++ saga-sdk-java/src/test/java/games/mythical/saga/sdk/client/executor/MockReservationExecutor.java
@@ -1,11 +1,15 @@
 package games.mythical.saga.sdk.client.executor;
 
+import games.mythical.saga.sdk.client.model.SagaItem;
 import lombok.Getter;
+
+import java.util.List;
 
 @Getter
 public class MockReservationExecutor extends MockBaseExecutor implements SagaReservationExecutor {
     private String traceId;
     private String reservationId;
+    private List<SagaItem> items;
 
     @Override
     public void onReservationCreated(String reservationId, String traceId) {
@@ -14,8 +18,9 @@ public class MockReservationExecutor extends MockBaseExecutor implements SagaRes
     }
 
     @Override
-    public void onReservationRedeemed(String reservationId, String traceId) {
+    public void onReservationRedeemed(String reservationId, List<SagaItem> items, String traceId) {
         this.reservationId = reservationId;
+        this.items = items;
         this.traceId = traceId;
     }
 
