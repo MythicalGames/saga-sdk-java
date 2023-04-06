623fb052dfa01b5b07a9250355503d107b2a1276
Kevin Wang <kevin.wang@mythical.games>
2023-04-06 17:05:02 -0500

    Clients in the SagaClientFactoryTest weren't shutting down properly

--- saga-sdk-java/src/test/java/games/mythical/saga/sdk/client/SagaClientFactoryTest.java
+++ saga-sdk-java/src/test/java/games/mythical/saga/sdk/client/SagaClientFactoryTest.java
@@ -1,20 +1,21 @@
 package games.mythical.saga.sdk.client;
 
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-
-import games.mythical.saga.sdk.client.executor.MockCurrencyExecutor;
-import games.mythical.saga.sdk.client.executor.MockItemExecutor;
-import games.mythical.saga.sdk.client.executor.MockItemTypeExecutor;
-import games.mythical.saga.sdk.client.executor.MockMetadataExecutor;
-import games.mythical.saga.sdk.client.executor.MockPlayerWalletExecutor;
-import games.mythical.saga.sdk.client.executor.MockReservationExecutor;
+import games.mythical.saga.sdk.client.executor.*;
 import games.mythical.saga.sdk.exception.SagaException;
+import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 public class SagaClientFactoryTest extends AbstractClientTest {
 
     private SagaClientFactory factory;
+    private static final List<AbstractSagaStreamClient> clientCleanupTargets = new LinkedList<>();
 
     @BeforeEach
     public void beforeEach() throws SagaException {
@@ -22,44 +23,78 @@ public class SagaClientFactoryTest extends AbstractClientTest {
         factory = setUpFactory();
     }
 
+    @AfterAll
+    public static void cleanup() throws InterruptedException {
+        for (var client : clientCleanupTargets) {
+            client.stop();
+            client.channel.awaitTermination(5, TimeUnit.SECONDS);
+        }
+    }
+
     @Test
     public void testCreateCurrencyClient() throws SagaException {
-        assertNotNull(factory.createSagaCurrencyClient());
-        assertNotNull(factory.createSagaCurrencyClient(MockCurrencyExecutor.builder().build()));
+        var client_1 = factory.createSagaCurrencyClient();
+        clientCleanupTargets.add(client_1);
+        assertNotNull(client_1);
+        var client_2 = factory.createSagaCurrencyClient(MockCurrencyExecutor.builder().build());
+        clientCleanupTargets.add(client_2);
+        assertNotNull(client_2);
     }
 
     @Test
     public void testCreateCurrencyTypeClient() throws SagaException {
-        assertNotNull(factory.createSagaCurrencyTypeClient());
+        var client_1 = factory.createSagaCurrencyTypeClient();
+        clientCleanupTargets.add(client_1);
+        assertNotNull(client_1);
     }
 
     @Test
     public void testCreateItemClient() throws SagaException {
-        assertNotNull(factory.createSagaItemClient());
-        assertNotNull(factory.createSagaItemClient(MockItemExecutor.builder().build()));
+        var client_1 = factory.createSagaItemClient();
+        clientCleanupTargets.add(client_1);
+        assertNotNull(client_1);
+        var client_2 = factory.createSagaItemClient(MockItemExecutor.builder().build());
+        clientCleanupTargets.add(client_2);
+        assertNotNull(client_2);
     }
 
     @Test
     public void testCreateItemTypeClient() throws SagaException {
-        assertNotNull(factory.createSagaItemTypeClient());
-        assertNotNull(factory.createSagaItemTypeClient(MockItemTypeExecutor.builder().build()));
+        var client_1 = factory.createSagaItemTypeClient();
+        clientCleanupTargets.add(client_1);
+        assertNotNull(client_1);
+        var client_2 = factory.createSagaItemTypeClient(MockItemTypeExecutor.builder().build());
+        clientCleanupTargets.add(client_2);
+        assertNotNull(client_2);
     }
 
     @Test
     public void testCreateMetadataClient() throws SagaException {
-        assertNotNull(factory.createSagaMetadataClient());
-        assertNotNull(factory.createSagaMetadataClient(MockMetadataExecutor.builder().build()));
+        var client_1 = factory.createSagaMetadataClient();
+        clientCleanupTargets.add(client_1);
+        assertNotNull(client_1);
+        var client_2 = factory.createSagaMetadataClient(MockMetadataExecutor.builder().build());
+        clientCleanupTargets.add(client_2);
+        assertNotNull(client_2);
     }
 
     @Test
     public void testCreatePlayerWalletClient() throws SagaException {
-        assertNotNull(factory.createSagaPlayerWalletClient());
-        assertNotNull(factory.createSagaPlayerWalletClient(new MockPlayerWalletExecutor()));
+        var client_1 = factory.createSagaPlayerWalletClient();
+        clientCleanupTargets.add(client_1);
+        assertNotNull(client_1);
+        var client_2 = factory.createSagaPlayerWalletClient(new MockPlayerWalletExecutor());
+        clientCleanupTargets.add(client_2);
+        assertNotNull(client_2);
     }
 
     @Test
     public void testCreateReservationClient() throws SagaException {
-        assertNotNull(factory.createSagaReservationClient());
-        assertNotNull(factory.createSagaReservationClient(new MockReservationExecutor()));
+        var client_1 = factory.createSagaReservationClient();
+        clientCleanupTargets.add(client_1);
+        assertNotNull(client_1);
+        var client_2 = factory.createSagaReservationClient(new MockReservationExecutor());
+        clientCleanupTargets.add(client_2);
+        assertNotNull(client_2);
     }
 }
