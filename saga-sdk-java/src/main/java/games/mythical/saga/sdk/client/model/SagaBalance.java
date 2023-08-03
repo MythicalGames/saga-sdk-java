8207d18aac87f5213fe2d4c7da8657f2c21c5517
Alex Moir <alex.moir@mythical.games>
2023-08-02 18:38:33 -0700

    The publisher balance is now properly set if the proto returns a value for this field or not.

--- saga-sdk-java/src/main/java/games/mythical/saga/sdk/client/model/SagaBalance.java
+++ saga-sdk-java/src/main/java/games/mythical/saga/sdk/client/model/SagaBalance.java
@@ -1,7 +1,5 @@
 package games.mythical.saga.sdk.client.model;
 
-import games.mythical.proto_util.ProtoUtil;
-import games.mythical.saga.sdk.proto.api.currencytype.PublisherBalanceProto;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -14,9 +12,4 @@ import lombok.NoArgsConstructor;
 public class SagaBalance {
     private String address;
     private String balanceInWei;
-
-    public static SagaBalance fromProto(PublisherBalanceProto proto) {
-        var balance = ProtoUtil.toDto(proto, SagaBalance.class);
-        return balance;
-    }
 }
