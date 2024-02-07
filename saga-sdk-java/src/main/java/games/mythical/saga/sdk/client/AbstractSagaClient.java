3bf2f21d9554a5d0aa153ea3d27bf88481bfc09c
olehkaliuzhnyi <149395854+olehkaliuzhnyi@users.noreply.github.com>
2024-02-07 11:07:41 +0200

    Update AbstractSagaClient.java

--- saga-sdk-java/src/main/java/games/mythical/saga/sdk/client/AbstractSagaClient.java
+++ saga-sdk-java/src/main/java/games/mythical/saga/sdk/client/AbstractSagaClient.java
@@ -11,6 +11,7 @@ import io.grpc.ManagedChannelBuilder;
 import io.grpc.Metadata;
 import lombok.extern.slf4j.Slf4j;
 
+
 import java.util.concurrent.Executor;
 import java.util.concurrent.TimeUnit;
 
